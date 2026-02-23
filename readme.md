3914a7a6ae4cf13928e66367650495c86fd972f6
hamdpac <hamdpac@localhost>
2026-02-23 16:40:01 +0100

    initial commit

---
+++ readme.md
@@ -0,0 +1,9 @@
+# string generator program
+this program generates random strings that are easy to speak out loud
+## why?
+well i get bored sometimes and this was the result of a little time working on this thing.
+## usage
+just run it with the argument of how many strings you want to generate. and the length of the string.
+## notes
+this is not exactly the best code ever, I would actualy argue the opposit. but it works, and it doesnt need to be great ore public but it is anyways(public, not great).	
+no pr's will be accepted, i do not think there will be any but just in case.
---
+++ src/main.rs
@@ -0,0 +1,81 @@
+use rand::prelude::*;
+const V: [char;6]= ['a', 'e', 'i', 'o', 'u', 'y'];
+const C: [char;20] = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z'];
+enum State {
+	StartV,
+	StartC,
+	StartC2,
+}
+
+fn generate(len: usize) -> String {
+	let mut rng = rand::rng();
+	let mut prevstate: Option<State> = None;
+	let mut buf = String::with_capacity(len);
+for _ in 0..len {
+	if prevstate.is_none() {
+	let is_v = rand::random_bool(0.5);
+	let next_char = if is_v {
+		V.choose(&mut rng)
+	}
+	else {
+		C.choose(&mut rng)
+	}.unwrap();
+	let state = if is_v {
+		State::StartV
+	}
+	else {
+		State::StartC
+	};
+buf.push(*next_char);
+prevstate = Some(state)
+	}
+	else {
+		let state = match prevstate.unwrap() {
+State::StartV => {
+	let next_char = C.choose(&mut rng).unwrap();
+	buf.push(*next_char);
+	State::StartC
+},
+State::StartC => {
+	let is_c = rng.random_bool(0.5);
+	let next_char = if is_c {
+		C.choose(&mut rng).unwrap()
+	}
+	else {
+		V.choose(&mut rng).unwrap()
+	};
+	buf.push(*next_char);
+if is_c {
+	State::StartC2
+}
+else {
+	State::StartV
+}
+},
+State::StartC2 => {
+	let next_char = V.choose(&mut rng).unwrap();
+	buf.push(*next_char);
+	State::StartV
+}
+		};
+		prevstate = Some(state)
+	}
+}
+buf
+}
+	fn main() {
+let count: usize = std::env::args()
+    .nth(1)
+    .expect("Missing argument")
+    .parse()
+    .expect("Invalid number");
+			let len: usize = std::env::args()
+    .nth(2)
+    .expect("Missing argument")
+    .parse()
+    .expect("Invalid number");
+	for i in 1..=count {
+let string = generate(len);
+println!("string number {} is: {}", i, string);
+}
+	}
